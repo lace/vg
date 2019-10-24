52565cc7902d3f76e0cd8b3a09eff2f58bd10295
Paul Melnikow <email@paulmelnikow.com>
2019-10-24 07:49:04 +0200

    Add `vg.nearest()`

---
+++ vg/test_nearest.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pytest
+from . import core as vg
+
+
+def test_nearest():
+    to_point = np.array([-1.0, 0.0, 0.0])
+
+    from_points = np.array([[1.0, -2.0, -3.0], [-1.0, -20.0, -30.0]])
+
+    point, index = vg.nearest(from_points, to_point, ret_index=True)
+
+    np.testing.assert_array_equal(point, from_points[0])
+    np.testing.assert_array_equal(index, 0)
