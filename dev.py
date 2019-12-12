eb29bab81fe17b0a66b262ae6f2b2d78d9186482
Paul Melnikow <github@paulmelnikow.com>
2019-12-12 13:26:49 -0500

    Build wheel (#101)

--- dev.py
+++ dev.py
@@ -75,7 +75,8 @@ def doc_open():
 @cli.command()
 def publish():
     execute("rm -rf dist/")
-    execute("python setup.py sdist")
+    execute("python3 setup.py sdist bdist_wheel")
+    execute("python2 setup.py bdist_wheel")
     execute("twine upload dist/*")
 
 
