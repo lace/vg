2c35fa53761c354d2758996af9e0f32a2af5a083
Paul Melnikow <github@paulmelnikow.com>
2020-04-22 16:30:44 -0400

    Incorporate `check_value_any()` and `columnize()` from polliwog, improve `euclidean_distance()` (#109)

--- vg/shape.py
+++ vg/shape.py
@@ -1,4 +1,4 @@
-def check_value(a, shape, **kwargs):
+def check_value(arr, shape, **kwargs):
     """
     Check that the given argument has the expected shape. Shape dimensions can
     be ints or -1 for a wildcard. The wildcard dimensions are returned, which
@@ -6,9 +6,9 @@ def check_value(a, shape, **kwargs):
     function.
 
     Args:
-        a (np.arraylike): An array-like input.
-        shape (list): Shape to validate. To require 3 by 1, pass `(3,)`. To
-            require n by 3, pass `(-1, 3)`.
+        arr (np.arraylike): An array-like input.
+        shape (list): Shape to validate. To require an array with 3 elements,
+            pass `(3,)`. To require n by 3, pass `(-1, 3)`.
         name (str): Variable name to embed in the error message.
 
     Returns:
@@ -36,25 +36,25 @@ def check_value(a, shape, **kwargs):
     else:
         preamble = "Expected an array"
 
-    if a is None:
+    if arr is None:
         raise ValueError("{} with shape {}; got None".format(preamble, shape))
     try:
-        len(a.shape)
+        len(arr.shape)
     except (AttributeError, TypeError):
         raise ValueError(
-            "{} with shape {}; got {}".format(preamble, shape, a.__class__.__name__)
+            "{} with shape {}; got {}".format(preamble, shape, arr.__class__.__name__)
         )
 
     # Check non-wildcard dimensions.
-    if len(a.shape) != len(shape) or any(
+    if len(arr.shape) != len(shape) or any(
         actual != expected
-        for actual, expected in zip(a.shape, shape)
+        for actual, expected in zip(arr.shape, shape)
         if not is_wildcard(expected)
     ):
-        raise ValueError("{} with shape {}; got {}".format(preamble, shape, a.shape))
+        raise ValueError("{} with shape {}; got {}".format(preamble, shape, arr.shape))
 
     wildcard_dims = [
-        actual for actual, expected in zip(a.shape, shape) if is_wildcard(expected)
+        actual for actual, expected in zip(arr.shape, shape) if is_wildcard(expected)
     ]
     if len(wildcard_dims) == 0:
         return None
@@ -62,6 +62,68 @@ def check_value(a, shape, **kwargs):
         return wildcard_dims[0]
     else:
         return tuple(wildcard_dims)
+
+
+# TODO-2.x: Remove kwargs hack when upgrading to Python 3.
+def check_value_any(arr, *shapes, **kwargs):
+    """
+    Check that the given argument has any of the expected shapes. Shape dimensons
+    can be ints or -1 for a wildcard.
+
+    Args:
+        arr (np.arraylike): An array-like input.
+        shape (list): Shape candidates to validate. To require an array with 3
+            elements, pass `(3,)`. To require n by 3, pass `(-1, 3)`.
+        name (str): Variable name to embed in the error message.
+
+    Returns:
+        object: The wildcard dimension of the matched shape (if one) or a tuple
+        of wildcard dimensions (if more than one). If the matched shape has no
+        wildcard dimensions, returns `None`.
+
+    Example:
+        >>> k = check_shape_any(points, (3,), (-1, 3), name="points")
+        >>> check_shape_any(
+                reference_points_of_lines,
+                (3,),
+                (-1 if k is None else k, 3),
+                name="reference_points_of_lines",
+            )
+    """
+    if len(shapes) == 0:
+        raise ValueError("At least one shape is required")
+    for shape in shapes:
+        try:
+            return check_value(arr, shape, name=kwargs.get("name", "arr"))
+        except ValueError:
+            pass
+
+    if "name" in kwargs:
+        preamble = "Expected {} to be an array".format(kwargs["name"])
+    else:
+        preamble = "Expected an array"
+
+    if len(shapes) == 1:
+        (shape_choices,) = shapes
+    else:
+        shape_choices = ", ".join(
+            shapes[:-2] + (" or ".join([str(shapes[-2]), str(shapes[-1])]),)
+        )
+
+    if arr is None:
+        raise ValueError("{} with shape {}; got None".format(preamble, shape_choices))
+    else:
+        try:
+            len(arr.shape)
+        except (AttributeError, TypeError):
+            raise ValueError(
+                "{} with shape {}; got {}".format(
+                    preamble, shape_choices, arr.__class__.__name__
+                )
+            )
+        raise ValueError(
+            "{} with shape {}; got {}".format(preamble, shape_choices, arr.shape)
+        )
 
 
 def check(locals_namespace, name, shape):
@@ -91,3 +153,28 @@ def check(locals_namespace, name, shape):
 
     """
     return check_value(locals_namespace[name], shape, name=name)
+
+
+def columnize(arr, shape=(-1, 3), name=None):
+    """
+    Helper for functions which may accept many stacks of three points (kx3)
+    returning a stack of results, or a single set of three points (3x1)
+    returning a single result.
+
+    Returns the points as kx3, and a `transform_result` function which can
+    be applied to the result. It picks off the first result in the 3x1 case.
+
+    Not limited to kx3; this can be used different dimensional shapes like
+    kx4, or higher dimensional shapes like kx3x3.
+    """
+    if not isinstance(shape, tuple):
+        raise ValueError("shape should be a tuple")
+    if len(shape) < 2:
+        raise ValueError("shape should have at least two dimensions")
+
+    check_value_any(arr, shape, shape[1:], name=name or "arr")
+
+    if arr.ndim == len(shape):
+        return arr, True, lambda x: x
+    else:
+        return arr.reshape(*shape), False, lambda x: x[0]
