28661491ca8a9dc62e34f7d98445d16052bb9a9e
Paul Melnikow <email@paulmelnikow.com>
2021-07-10 16:07:17 -0400

    Reformat

--- vg/test_aligned_with.py
+++ vg/test_aligned_with.py
@@ -10,7 +10,9 @@ def test_aligned_with():
     np.testing.assert_array_equal(vg.aligned_with(v1, along=vg.basis.z), v1)
     np.testing.assert_array_equal(vg.aligned_with(neg_v1, along=vg.basis.z), v1)
 
-    np.testing.assert_array_equal(vg.aligned_with(v1, along=vg.basis.z, reverse=True), neg_v1)
+    np.testing.assert_array_equal(
+        vg.aligned_with(v1, along=vg.basis.z, reverse=True), neg_v1
+    )
     np.testing.assert_array_equal(
         vg.aligned_with(neg_v1, along=vg.basis.z, reverse=True), neg_v1
     )
@@ -20,6 +22,5 @@ def test_orient_is_alias_for_aligned_with():
     v1 = np.array([1.0, 2.0, 3.0])
     with pytest.deprecated_call():
         np.testing.assert_array_equal(
-            vg.orient(v1, along=vg.basis.z),
-            vg.aligned_with(v1, along=vg.basis.z)
+            vg.orient(v1, along=vg.basis.z), vg.aligned_with(v1, along=vg.basis.z)
         )
