d8de55ca6db8bb7beaac2ce2ea1fc0b2a67acfac
Paul Melnikow <email@paulmelnikow.com>
2021-07-10 17:34:15 -0400

    Deprecate matrix functions

--- vg/test_matrix_transform.py
+++ vg/test_matrix_transform.py
@@ -16,19 +16,26 @@ transform = np.array(
 def test_apply_homogeneous():
     point = np.array([5.0, 0.0, 1.0])
     expected_point = np.array([15.0, 0.0, 2.0])
-    np.testing.assert_array_equal(apply_transform(point, transform), expected_point)
+    with pytest.deprecated_call():
+        np.testing.assert_array_equal(apply_transform(point, transform), expected_point)
 
 
 def test_apply_homogeneous_stacked():
     points = np.array([[1.0, 2.0, 3.0], [5.0, 0.0, 1.0]])
     expected_points = np.array([[3.0, 1.0, 6.0], [15.0, 0.0, 2.0]])
-    np.testing.assert_array_equal(apply_transform(points, transform), expected_points)
+    with pytest.deprecated_call():
+        np.testing.assert_array_equal(
+            apply_transform(points, transform), expected_points
+        )
 
 
 def test_apply_homogeneous_error():
-    with pytest.raises(ValueError, match="Transformation matrix should be 4x4"):
-        apply_transform(np.array([1.0, 2.0, 3.0]), np.array([1.0]))
-    with pytest.raises(ValueError, match=r"Vertices should be \(3,\) or Nx3"):
-        apply_transform(np.array([1.0, 2.0]), transform)
-    with pytest.raises(ValueError, match="Not sure what to do with 3 dimensions"):
-        apply_transform(np.array([[[1.0, 2.0, 3.0]]]), transform)
+    with pytest.deprecated_call():
+        with pytest.raises(ValueError, match="Transformation matrix should be 4x4"):
+            apply_transform(np.array([1.0, 2.0, 3.0]), np.array([1.0]))
+    with pytest.deprecated_call():
+        with pytest.raises(ValueError, match=r"Vertices should be \(3,\) or Nx3"):
+            apply_transform(np.array([1.0, 2.0]), transform)
+    with pytest.deprecated_call():
+        with pytest.raises(ValueError, match="Not sure what to do with 3 dimensions"):
+            apply_transform(np.array([[[1.0, 2.0, 3.0]]]), transform)
--- vg/test_matrix_unpad.py
+++ vg/test_matrix_unpad.py
@@ -4,25 +4,33 @@ from .matrix import unpad
 
 
 def test_unpad():
-    np.testing.assert_array_equal(
-        unpad(
-            np.array([[1.0, 2.0, 3.0, 1.0], [2.0, 3.0, 4.0, 1.0], [5.0, 6.0, 7.0, 1.0]])
-        ),
-        np.array([[1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [5.0, 6.0, 7.0]]),
-    )
+    with pytest.deprecated_call():
+        np.testing.assert_array_equal(
+            unpad(
+                np.array(
+                    [[1.0, 2.0, 3.0, 1.0], [2.0, 3.0, 4.0, 1.0], [5.0, 6.0, 7.0, 1.0]]
+                )
+            ),
+            np.array([[1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [5.0, 6.0, 7.0]]),
+        )
 
     # NB: on windows, the sizes here will render as 3L, not 3:
-    with pytest.raises(
-        ValueError, match=r"^Invalid shape \(3L?, 3L?\): unpad expects nx4$"
-    ):
-        unpad(np.array([[1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [5.0, 6.0, 7.0]]))
+    with pytest.deprecated_call():
+        with pytest.raises(
+            ValueError, match=r"^Invalid shape \(3L?, 3L?\): unpad expects nx4$"
+        ):
+            unpad(np.array([[1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [5.0, 6.0, 7.0]]))
 
-    with pytest.raises(
-        ValueError, match=r"^Invalid shape \(4L?,\): unpad expects nx4$"
-    ):
-        unpad(np.array([1.0, 2.0, 3.0, 4.0]))
+    with pytest.deprecated_call():
+        with pytest.raises(
+            ValueError, match=r"^Invalid shape \(4L?,\): unpad expects nx4$"
+        ):
+            unpad(np.array([1.0, 2.0, 3.0, 4.0]))
 
-    with pytest.raises(ValueError, match="Expected a column of ones"):
-        unpad(
-            np.array([[1.0, 2.0, 3.0, 1.0], [2.0, 3.0, 4.0, 1.0], [5.0, 6.0, 7.0, 3.0]])
-        )
+    with pytest.deprecated_call():
+        with pytest.raises(ValueError, match="Expected a column of ones"):
+            unpad(
+                np.array(
+                    [[1.0, 2.0, 3.0, 1.0], [2.0, 3.0, 4.0, 1.0], [5.0, 6.0, 7.0, 3.0]]
+                )
+            )
