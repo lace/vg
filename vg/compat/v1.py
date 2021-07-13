64d96459cdfd6052d015c27cad5a6f60f1eb66d2
Paul Melnikow <email@paulmelnikow.com>
2021-07-13 17:12:23 -0400

    lgtm ignore

--- vg/compat/v1.py
+++ vg/compat/v1.py
@@ -1,3 +1,3 @@
-from .. import matrix  # noqa: F401, F403
-from .. import shape  # noqa: F401, F403
+from .. import matrix  # noqa: F401, F403 lgtm[py/unused-import]
+from .. import shape  # noqa: F401, F403 lgtm[py/unused-import]
 from ..core import *  # noqa: F401, F403
