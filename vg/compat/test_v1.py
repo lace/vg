781e86a1b843ba3c4f001eeddcfa1785c3e3f2d9
Paul Melnikow <github@paulmelnikow.com>
2021-07-13 17:33:03 -0400

    Fix `vg.compat.v1.shape.*` and `vg.compat.v1.matrix.*` (#166)

--- vg/compat/test_v1.py
+++ vg/compat/test_v1.py
@@ -13,6 +13,27 @@ def test_v1_has_constants():
     np.testing.assert_array_equal(vg.basis.x, np.array([1, 0, 0]))
 
 
+def test_v1_has_shape_check():
+    input_value = np.zeros(3)
+    vg.shape.check(locals(), "input_value", (3,))
+
+
+def test_v1_has_matrix_transform():
+    scale_factor = np.array([3.0, 0.5, 2.0])
+    transform = np.array(
+        [
+            [scale_factor[0], 0, 0, 0],
+            [0, scale_factor[1], 0, 0],
+            [0, 0, scale_factor[2], 0],
+            [0, 0, 0, 1],
+        ]
+    )
+    point = np.array([5.0, 0.0, 1.0])
+    expected_point = np.array([15.0, 0.0, 2.0])
+    with pytest.deprecated_call():
+        np.testing.assert_array_equal(vg.matrix.transform(point, transform), expected_point)
+
+
 def test_v1_orient_is_alias_for_aligned_with():
     v1 = np.array([1.0, 2.0, 3.0])
     with pytest.deprecated_call():
