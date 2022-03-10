61752f73c51fb3221b766eac75c00adcca9a9295
Paul Melnikow <email@paulmelnikow.com>
2022-03-10 14:11:55 -0500

    Reformat

--- vg/test_euclidean_distance.py
+++ vg/test_euclidean_distance.py
@@ -6,7 +6,7 @@ import vg
 def test_euclidean_distance():
     v1 = np.array([1, 1, 0])
     v2 = np.array([2, -1, 5])
-    expected = math.sqrt(1 ** 2 + 2 ** 2 + 5 ** 2)
+    expected = math.sqrt(1**2 + 2**2 + 5**2)
     result = vg.euclidean_distance(v1, v2)
     np.testing.assert_almost_equal(result, expected)
     assert isinstance(result, float)
@@ -17,9 +17,9 @@ def test_euclidean_distance_stacked():
     v2s = np.array([[2, -1, 5], [3, 4, 0], [-1, 0, 6]])
     expected = np.array(
         [
-            math.sqrt(1 ** 2 + 2 ** 2 + 5 ** 2),
-            math.sqrt(4 ** 2 + 4 ** 2),
-            math.sqrt(1 ** 2 + 1 ** 2),
+            math.sqrt(1**2 + 2**2 + 5**2),
+            math.sqrt(4**2 + 4**2),
+            math.sqrt(1**2 + 1**2),
         ]
     )
     np.testing.assert_array_almost_equal(vg.euclidean_distance(v1s, v2s), expected)
@@ -30,9 +30,9 @@ def test_euclidean_distance_mixed():
     v2 = np.array([2, -1, 5])
     expected = np.array(
         [
-            math.sqrt(1 ** 2 + 2 ** 2 + 5 ** 2),
-            math.sqrt(3 ** 2 + 1 ** 2 + 5 ** 2),
-            math.sqrt(2 ** 2 + 1 ** 2),
+            math.sqrt(1**2 + 2**2 + 5**2),
+            math.sqrt(3**2 + 1**2 + 5**2),
+            math.sqrt(2**2 + 1**2),
         ]
     )
     np.testing.assert_array_almost_equal(vg.euclidean_distance(v1s, v2), expected)
