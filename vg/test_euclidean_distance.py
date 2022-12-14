dd4913d9f0bb6262a5ae6c6a0118731e834e7675
Jonathan Levin <jon@curvewise.com>
2022-12-14 11:30:15 -0500

    fix other instance

--- vg/test_euclidean_distance.py
+++ vg/test_euclidean_distance.py
@@ -32,7 +32,7 @@ def test_euclidean_distance_mixed():
         [
             math.sqrt(1**2 + 2**2 + 5**2),
             math.sqrt(3**2 + 1**2 + 5**2),
-            math.sqrt(2**2 + 1**2),
+            math.hypot(2, 1),
         ]
     )
     np.testing.assert_array_almost_equal(vg.euclidean_distance(v1s, v2), expected)
