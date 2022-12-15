16799ffcca09ab624a209c5052307a7e2ae6a6ef
Jonathan Levin <jon@curvewise.com>
2022-12-14 19:32:17 -0500

    sort lists

--- vg/test_helpers.py
+++ vg/test_helpers.py
@@ -1,2 +1,2 @@
 def get_imported_names(module):
-    return [name for name in module.__all__ if not name.startswith("_")]
+    return sorted([name for name in module.__all__ if not name.startswith("_")])
